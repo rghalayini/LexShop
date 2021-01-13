2e3e7b4f4b545909b797fc0f9450977cdda001d0
Remy Ghalayini <remyghalayini@gmail.com>
2021-01-13 17:22:19 +0100

    Addedupload images

--- LexShop/LexShop.WebUI/Controllers/ProductManagerController.cs
+++ LexShop/LexShop.WebUI/Controllers/ProductManagerController.cs
@@ -7,6 +7,7 @@ using LexShop.Core.Models;
 using LexShop.DataAccess.InMemory;
 using LexShop.Core.ViewModels;
 using LexShop.Core.Contracts;
+using System.IO;
 
 namespace LexShop.WebUI.Controllers
 {
@@ -35,7 +36,7 @@ namespace LexShop.WebUI.Controllers
 
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -43,6 +44,11 @@ namespace LexShop.WebUI.Controllers
             }
             else
             {
+                if(file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//Images//") + product.Image);
+                }
                 context.Insert(product);
                 context.Commit();
                 return RedirectToAction("Index");
@@ -64,7 +70,7 @@ namespace LexShop.WebUI.Controllers
             }
         }
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
@@ -77,9 +83,14 @@ namespace LexShop.WebUI.Controllers
                 {
                     return View(product);
                 }
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//Images//") + productToEdit.Image);
+                }
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
+
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
 
