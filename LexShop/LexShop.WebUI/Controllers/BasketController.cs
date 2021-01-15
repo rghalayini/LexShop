46eef5f83e427d382e9c94daef524096aea8c09f
Remy Ghalayini <remyghalayini@gmail.com>
2021-01-15 17:38:50 +0100

    Added basket model

---
+++ LexShop/LexShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LexShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+        public BasketController()
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
+
+        // GET: Basket
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);           
+        }
+    }
+}
