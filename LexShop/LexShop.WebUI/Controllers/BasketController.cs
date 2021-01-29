a3265ee75b42d4fd10d72bab073e7d6a73fdb550
Remy Ghalayini <remyghalayini@gmail.com>
2021-01-29 17:15:17 +0100

    added order and checkout model

--- LexShop/LexShop.WebUI/Controllers/BasketController.cs
+++ LexShop/LexShop.WebUI/Controllers/BasketController.cs
@@ -1,41 +1,79 @@
 ﻿using LexShop.Core.Contracts;
+using LexShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace LexShop.WebUI.Controllers
+namespace MyShop.WebUI.Controllers
 {
+    /*
+    9. After the user create the basket... We will use this basket controller to then create the order.
+    We also will add three new endpoints at the bottom of this page.
+    */
     public class BasketController : Controller
     {
         IBasketService basketService;
-
-        public BasketController(IBasketService BasketService)
+        IOrderService orderService;
+        public BasketController(IBasketService BasketService, IOrderService OrderService)
         {
             this.basketService = BasketService;
+            this.orderService = OrderService;
         }
-
-        // GET: BasketController2
+        // GET: Basket2
         public ActionResult Index()
         {
             var model = basketService.GetBasketItems(this.HttpContext);
             return View(model);
         }
+
         public ActionResult AddToBasket(string Id)
         {
             basketService.AddToBasket(this.HttpContext, Id);
+
             return RedirectToAction("Index");
         }
+
         public ActionResult RemoveFromBasket(string Id)
         {
             basketService.RemoveFromBasket(this.HttpContext, Id);
+
             return RedirectToAction("Index");
         }
+
         public PartialViewResult BasketSummary()
         {
             var basketSummary = basketService.GetBasketSummary(this.HttpContext);
+
             return PartialView(basketSummary);
+        }
+        //Create a checkout view(partial view) and set the template to a create template and make sure the model class is : Order (LexShop.Core.Models)
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Checkout(Order order)
+        {
+            var basketItems = basketService.GetBasketItems(this.HttpContext);
+            order.OrderStatus = "Order Created";
+
+            //TODO: process payment...
+
+            order.OrderStatus = "Payment Processed";
+            orderService.CreateOrder(order, basketItems);
+            //clear the basket
+            basketService.ClearBasket(this.HttpContext);
+
+            //And finally redirect the user to the thank you page with order id.
+            return RedirectToAction("Thankyou", new { OrderId = order.Id });
+        }
+        //create view (partial view) for the thank you page. Template: Empty
+        public ActionResult ThankYou(string OrderId)
+        {
+            ViewBag.OrderId = OrderId;
+            return View();
         }
     }
 }
