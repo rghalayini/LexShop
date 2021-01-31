ca9b800aa3f6399363b8217ba50e7e105c1f7ae3
Remy Ghalayini <remyghalayini@gmail.com>
2021-01-31 17:42:41 +0100

    Added order items to the admin page

--- LexShop/LexShop.Core/Contracts/IOrderService.cs
+++ LexShop/LexShop.Core/Contracts/IOrderService.cs
@@ -12,5 +12,8 @@ namespace LexShop.Core.Contracts
     public interface IOrderService
     {
         void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems);
+        List<Order> GetOrderList();
+        Order GetOrder(string Id);
+        void UpdateOrder(Order updatedOrder);
     }
 }
---
+++ LexShop/LexShop.WebUI/Controllers/OrderManagerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LexShop.Core.Contracts;
+using LexShop.Core.Models;
+
+namespace LexShop.WebUI.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        IOrderService orderService;
+        public OrderManagerController(IOrderService OrderService)
+        {
+            this.orderService = OrderService;
+        }
+
+
+
+        /// <summary>
+        /// Add a view to the index page
+        /// View name: Index
+        /// Template: List
+        /// Model class: Order (LexShop.Core.Models)
+
+
+        // GET: OrderManager
+        // index page will return our list of orders
+        public ActionResult Index()
+        {
+
+            List<Order> orders = orderService.GetOrderList();
+
+            return View(orders);
+        }
+        // Get individual order
+        public ActionResult UpdateOrder(string Id)
+        {
+            ViewBag.StatusList = new List<string>() {
+                "Order Created",
+                "Payment Processed",
+                "Order Shipped",
+                "Order Complete"
+            };
+            Order order = orderService.GetOrder(Id);
+            return View(order);
+        }
+        [HttpPost]
+        public ActionResult UpdateOrder(Order updatedOrder, string Id)
+        {
+            Order order = orderService.GetOrder(Id);
+
+            //change order status
+            order.OrderStatus = updatedOrder.OrderStatus;
+            orderService.UpdateOrder(order);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
